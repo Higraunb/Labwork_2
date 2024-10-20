cd9b72b9c67d7bfee30ca515ca2dbf799d086c4a
higraunb <bear.porf@gmail.com>
2024-10-20 19:49:59 +0300

    ladawork2

--- Project1/laba2.h
+++ Project1/laba2.h
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <stdlib.h>
 
+int counter = 0;
+double auxiliary_variable = 0;
+clock_t begin = 0, end = 0;
 
 double make_random_number(double min_ch, double max_ch, int sid_variable)
 {
@@ -13,10 +16,11 @@ double make_random_number(double min_ch, double max_ch, int sid_variable)
 }
 void Bubble_Sort(double *array,int len_of_array)
 {
-	double auxiliary_variable;
-	for (int counter = 0; counter < len_of_array - 1; counter++)
+    int counter = 0;
+    int counter_2 = len_of_array - 2;
+	for (counter = 0; counter < len_of_array - 1; counter++)
 	{
-		for (int counter_2 = len_of_array - 2; counter_2 >= counter; counter_2--)
+		for (counter_2 = len_of_array - 2; counter_2 >= counter; counter_2--)
 		{
 			if (array[counter_2] > array[counter_2 + 1])
 			{
@@ -31,7 +35,7 @@ void Insertion_Sort(double *array, int len_of_array)
 {
     double newElement;
 	int location;
-    for (int counter = 1; counter < len_of_array; counter++)
+    for (counter = 1; counter < len_of_array; counter++)
     {
         newElement = array[counter];
         location = counter - 1;
@@ -41,6 +45,28 @@ void Insertion_Sort(double *array, int len_of_array)
             location = location - 1;
         }
         array[location + 1] = newElement;
+    }
+}
+void Choice_Sort(double *array, int len_of_array)
+{
+    int counter_2 = 0;
+    double auxiliary_varibale_2 = 0;
+    for (counter = 0; counter < len_of_array; counter++)
+    {
+        auxiliary_variable = counter;
+        for (counter_2 = counter + 1; counter_2 < len_of_array; counter_2++)
+        {
+            if (array[counter_2] < array[counter])
+            {
+                auxiliary_variable = array[counter_2];
+            }
+        }
+        if (auxiliary_variable != counter)
+        {
+            array[counter] = auxiliary_varibale_2;
+            array[counter] = array[(int)auxiliary_variable];
+            array[(int)auxiliary_variable] = auxiliary_varibale_2;
+        }
     }
 }
 void Quick_Sort(double *array, int left, int right)
@@ -76,4 +102,74 @@ void Quick_Sort(double *array, int left, int right)
     if (right > index)
         Quick_Sort(array, index + 1, right);
 }
-
+void Merge_Sort(double *array, int len_of_array)
+{
+    int mid_of_array = len_of_array / 2; // находим середину сортируемой последовательности
+    if (len_of_array % 2 == 1)
+    {
+       mid_of_array++;
+    }
+    int move = 1; // шаг
+    // выделяем память под формируемую последовательность
+    double *array_for_sequence = (double*)malloc(len_of_array * sizeof(double));
+    int step;
+    while (move < len_of_array)
+    {
+        step = move;
+        int first_path_index = 0;   // индекс первого пути
+        int second_path_index = mid_of_array; // индекс второго пути
+        int element_index = 0;   // индекс элемента в результирующей последовательности
+        while (step <= mid_of_array)
+        {
+            while ((first_path_index < step) && (second_path_index < len_of_array) && (second_path_index < (mid_of_array + step)))
+            { // пока не дошли до конца пути
+              // заполняем следующий элемент формируемой последовательности
+              // меньшим из двух просматриваемых
+                if (array[first_path_index] < array[second_path_index])
+                {
+                    array_for_sequence[element_index] = array[first_path_index];
+                    first_path_index++; element_index++;
+                }
+                else {
+                    array_for_sequence[element_index] = array[second_path_index];
+                    second_path_index++; element_index++;
+                }
+            }
+            while (first_path_index < step)
+            { // переписываем оставшиеся элементы первого пути (если второй кончился раньше)
+                array_for_sequence[element_index] = array[first_path_index];
+                first_path_index++; element_index++;
+            }
+            while ((second_path_index < (mid_of_array + step)) && (second_path_index < len_of_array))
+            {  // переписываем оставшиеся элементы второго пути (если первый кончился раньше)
+                array_for_sequence[element_index] = array[second_path_index];
+                second_path_index++; element_index++;
+            }
+            step = step + move; // переходим к следующему этапу
+        }
+        step = move * 2;
+        // Переносим упорядоченную последовательность (промежуточный вариант) в исходный массив
+        for (first_path_index = 0; first_path_index < len_of_array; first_path_index++)
+        {
+            array[first_path_index] = array_for_sequence[first_path_index];
+        }
+    }
+}
+void Shell_Sort(double* array, int len_of_array)
+{
+    int counter_2;
+    for (int step = len_of_array / 2; step > 0; step /= 2)
+    {
+        for (counter = step; counter < len_of_array; counter += 1)
+        {
+            counter_2 = counter;
+            while (counter_2 >= step && array[counter_2 - step] > array[counter])
+            {
+                auxiliary_variable = array[counter_2];
+                array[counter_2] = array[counter_2 - step];
+                array[counter_2 - step] = auxiliary_variable;
+                counter_2 -= step;      
+            }
+        }
+    }
+}
--- Project1/program1.c
+++ Project1/program1.c
@@ -1,4 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define __GOT_SECURE_LIB__
+#define CLOCK_PER_SEC
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -7,11 +9,16 @@
 #include "laba2.h"
 int main()
 {
+
 	FILE* file_of_numbers = 0;
-	int count_of_numbers = 0, number_input_selection = 0;
+	int count_of_numbers = 0, number_input_selection = 0, user_selection_sort = 0,
+		user_selection = 0, size_of_file = 0, user_selection_reset = 0;
 	char Filename[10];
-	double min_ch = 0, max_ch = 0, random_number = 0, entered_number = 1,s = 0 , *array_of_random_number = (double*)malloc(count_of_numbers * sizeof(double));
-	printf("if you want to enter random numbers, enter 1, if you want to enter numbers from the keyboard, enter 2, if you want to enter numbers from a file, enter 3\n");
+	double min_ch = 0, max_ch = 0, random_number = 0, entered_number = 1, s = 0,
+		* array_of_number = 0, time_spend = 0.0;
+back:
+	printf("if you want to enter random numbers, enter 1\nif you want to "
+		"enter numbers from the keyboard, enter 2\nif you want to enter numbers from a file, enter 3\n");
 	scanf("%d", &number_input_selection);
 	if (number_input_selection == 1)
 	{
@@ -21,35 +28,20 @@ int main()
 		scanf("%lf", &max_ch);
 		printf("Enter the count of generated numbers: ");
 		scanf("%d", &count_of_numbers);
-		array_of_random_number = (double*)malloc(count_of_numbers * sizeof(double));
-		if (array_of_random_number != NULL)
+		array_of_number = (double*)malloc(count_of_numbers * sizeof(double));
+		if (array_of_number != NULL)
 		{
 			for (int counter = 0; counter < count_of_numbers; counter++)
 			{
-				array_of_random_number[counter] = make_random_number(min_ch, max_ch, counter);
+				array_of_number[counter] = make_random_number(min_ch, max_ch, counter);
 			}
-		}
-		Bubble_Sort(array_of_random_number, count_of_numbers);
-		for (int counter = 0; counter < count_of_numbers; counter++)
-		{
-			printf("%lg\n" , array_of_random_number[counter]);
-		}
-		printf("\n");
-		Insertion_Sort(array_of_random_number, count_of_numbers);
-		for (int counter = 0; counter < count_of_numbers; counter++)
-		{
-			printf("%lg\n", array_of_random_number[counter]);
-		}
-		printf("\n");
-		Quick_Sort(array_of_random_number,0 , count_of_numbers - 1);
-		for (int counter = 0; counter < count_of_numbers; counter++)
-		{
-			printf("%lg\n", array_of_random_number[counter]);
 		}
 	}
 	else if (number_input_selection == 2)
 	{
-		printf("To finish entering, press 0\n");
+		count_of_numbers = 0;
+		array_of_number = (double*)malloc(100000 * sizeof(double));
+		printf("Enter numbers, To finish entering, press 0\n");
 		while (entered_number != 0)
 		{
 			scanf("%lg", &entered_number);
@@ -59,32 +51,123 @@ int main()
 			}
 			if (entered_number != 0)
 			{
-				array_of_random_number = malloc(1000 * sizeof(double));
-				*array_of_random_number = entered_number;
+				array_of_number[count_of_numbers] = entered_number;
+				count_of_numbers++;
 			}
 			else
 			{
-				printf("Eror");
+				printf("Eror\n");
 				return 1;
 			}
 		}
 	}
 	else if (number_input_selection == 3)
 	{
+		printf("Enter file name\n");
 		scanf("%s", &Filename);
-		int fg = 0;
 		file_of_numbers = fopen(Filename, "r");
-		for (int i = 0; i < 4; i++)
+		fseek(file_of_numbers, 0, SEEK_END);
+		size_of_file = ftell(file_of_numbers);
+		array_of_number = (double*)malloc(size_of_file * sizeof(double));
+		for (counter = 0; counter < size_of_file; counter++)
 		{
-			fscanf(file_of_numbers, "%d", &fg);
-			printf("%d", fg);
+			fscanf(file_of_numbers, "%lg", array_of_number[counter]);
 		}
+		count_of_numbers = size_of_file;
+		fclose(file_of_numbers);
 	}
 	else
 	{
-		printf("Eror");
+		printf("Eror\n");
 		return 2;
 	}
-	free(array_of_random_number);
+while (user_selection_reset == 0)
+{
+printf("if you want to print an array press 1\n"
+	"if you want to sort the array press 2\n"
+	"if you want to go back to the beginning press 3\n"
+	"if you want to exit the program press 4\n");
+	scanf("%d", &user_selection);
+	if (user_selection == 1)
+	{
+		for (counter = 0; counter < count_of_numbers; counter++)
+		{
+			printf("%lg\n", array_of_number[counter]);
+		}
+	}
+	else if (user_selection == 2)
+	{
+		printf("choose which sort to perform - \n Bubble_Sort - 1\n Choice_Sort - 2\n, "
+			"Insertion_Sort - 3\n Merge_Sort - 4\n Quick_Sort - 5\n Shell_Sort - 6\n подсчетом - 7\n");
+		user_selection = 0;
+		scanf("%d", &user_selection_sort);
+
+		if (user_selection_sort == 1)
+		{
+			time_spend = 0.0;
+			begin = clock();
+			Bubble_Sort(array_of_number, count_of_numbers);
+			end = clock();
+			time_spend += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("%lg\n", time_spend);
+		}
+		else if (user_selection_sort == 2)
+		{
+			time_spend = 0.0;
+			begin = clock();
+			Choice_Sort(array_of_number, count_of_numbers);
+			end = clock();
+			time_spend += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("%lg\n", time_spend);
+		}
+		else if (user_selection_sort == 3)
+		{
+			time_spend = 0.0;
+			begin = clock();
+			Insertion_Sort(array_of_number, count_of_numbers);
+			end = clock();
+			time_spend += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("%lg\n", time_spend);
+		}
+		else if (user_selection_sort == 4)
+		{
+			time_spend = 0.0;
+			begin = clock();
+			Merge_Sort(array_of_number, count_of_numbers);
+			end = clock();
+			time_spend += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("%lg\n", time_spend);
+		}
+		else if (user_selection_sort == 5)
+		{
+			time_spend = 0.0;
+			begin = clock();
+			Quick_Sort(array_of_number, array_of_number[1], array_of_number[count_of_numbers]);
+			end = clock();
+			time_spend += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("%lg\n", time_spend);
+		}
+		else if (user_selection_sort == 6)
+		{
+			time_spend = 0.0;
+			begin = clock();
+			Shell_Sort(array_of_number, count_of_numbers);
+			end = clock();
+			time_spend += (double)(end - begin) / CLOCKS_PER_SEC;
+			printf("%lg\n", time_spend);
+		}
+		else
+			printf("Eror\n");
+	}
+	else if (user_selection == 3)
+	{
+		goto back;
+	}
+	else if (user_selection == 4)
+	{
+		break;
+	}
+}
+	free(array_of_number);
 	return 0;
 }
